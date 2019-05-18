7b323b6451d641acfc7cd28233cc9bb63f678925
“LM” <lmira013@fiu.edu>
2019-05-18 11:31:21 -0400

    in progress

---
+++ Resources/organ_transplant.sqlite.sql
@@ -0,0 +1,20 @@
+CREATE database project_db;
+USE project_db;
+/*
+CREATE TABLE organ_transplant (
+id INT NOT NULL PRIMARY KEY,
+organ_transplantation_type VARCHAR(80),
+organ_procurement_organization_name VARCHAR(80),
+organ_procurement_organization_city VARCHAR(80),
+county VARCHAR(40), 
+state VARCHAR(40),
+state_abbreviation VARCHAR(10),
+data_warehouse_record_create_date_text DATE,
+geocoding__primary_x_coordinate FLOAT, 
+geocoding__primary_y_coordinate FLOAT)
+;
+*/
+
+SHOW TABLES;
+SELECT * FROM project_db;
+
