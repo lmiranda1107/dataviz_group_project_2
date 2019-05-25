1fb4501b49c6d21dd660944bd68e4a63114fbbb9
“LM” <lmira013@fiu.edu>
2019-05-24 23:11:23 -0400

    restrutured app to use routes

--- app.py
+++ app.py
@@ -8,19 +8,28 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
-##################################################
 from flask import Flask, jsonify, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(
+    __name__, 
+    template_folder='templates/light-bootstrap-dashboard-master',
+    static_url_path='/assets',
+    static_folder='templates/light-bootstrap-dashboard-master/static'
+)
 
 #################################################
 # Database Setup
 #################################################
 
-app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///organ_transplant.db"
 # Create DB object to pass Flask app to it - SQLAlchemy object that can be used in Flask
 db = SQLAlchemy(app)
 
+#Create database model (emoji exercise 15.3.3)
 #Create a class that defines what the table is and what columns are on the table
 class Organ_data(db.Model):
     __tablename__ = 'organ_data'
@@ -39,39 +48,54 @@ class Organ_data(db.Model):
     def __repr__(self):
         return '<Organ_data %r>' % (self.name)
 
-# create route that renders index.html template
+#Create route that renders dashboard.html template
 @app.route("/")
 def home():
-    return render_template("dashboard.html")
+    active_tab = "dashboard"
+    return render_template("dashboard.html", active = active_tab)
+
+@app.route("/etl")
+def etl():
+    active_tab = "etl"
+    return render_template("user.html", active = active_tab )
+
+
+@app.route("/table")
+def table():
+    active_tab = "table"
+    return render_template("table.html", active = active_tab )
 
 @app.route("/map")
 def map():
-    return render_template("map.html")
+    active_tab = "maps"
+    return render_template("maps.html",  active = active_tab)
+
+@app.route("/chartdata", methods=['POST'])
+def chartdata():
+    print("pepee")
+
 
 # create route that returns data for plotting
-@app.route("/Top_organs")
-def top_organs():
-    #this will return the top 5 organs based on most transplants done
-    #results = db.session.query(Organ_data.organ_transplantation_type, func.count(Organ_data.type)).group_by(Organ_data.type).limit(5).all()
-    results = db.session.query(Organ_data.organ_transplantation_type, Organ_data.count(Organ_data.type)).group_by(Organ_data.type).limit(5).all()
+@app.route("/Top_organs_chart")
+def top_organs_chart():
+   # this will return the top 5 organs based on most transplants done
+    results = db.session.query(Organ_data.organ_transplantation_type, func.count(Organ_data.organ_transplantation_type)).group_by(Organ_data.organ_transplantation_type).all()
+#   results = db.session.query(Organ_data.organ_transplantation_type, Organ_data.count(Organ_data.type)).group_by(Organ_data.type).limit(5).all()
     
+    organ_type = [result[0] for result in results]
+    id = [result[1] for result in results]
+
     trace = {
-        "x": Organ_data,
-        "y": Organ_data.count,
+        "x": organ_type,
+        "y": id,
         "type": "bar"
     }
-    
+
     return jsonify(trace)
-  #  Organ_data = [result[0] for result in results]
-   # age = [result[1] for result in results]
-'''
-    trace = {
-        "x": Organ_data,
-        "y": age,
-        "type": "bar"
-    }
-'''
-    
+
+
+if __name__ == "__main__":
+    app.run()
 
 #db.reflect()
 # reflect an existing database into a new model
@@ -88,5 +112,20 @@ def top_organs():
 
 
 
+#if __name__ == "__main__":
+ #   app.run()
+
+
+'''
+#Create 1st Chart (Donut)
+@app.route("/ct-chart")
+def organ_data_donut():
+    
+    # Query for the top 5 organ data
+    count_results = 
+    count = db.session.query(Organ_data.ct-chart, Emoji.score).\
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+'''
